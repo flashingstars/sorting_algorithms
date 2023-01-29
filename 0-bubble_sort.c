67bd36f1502b333586abb66c951754f4ebd3f3d4
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-01-29 10:18:12 -0800

    Sorting algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swaps two integers in an array.
+ * @a: The first integer to swap
+ * @b: The second integer to swap
+ */
+void swap_ints(int *a, int *b)
+{
+	int temp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - function that sorts an array of integers
+ * @array: pointer to an array of unsorted integers
+ * @size: the size of array
+ *
+ * Return: nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+This is a folder that deals with tasks that sort data while checking their speed and efficiency.
+
+0. Bubble sort
+
+1. Insertion sort.
+
+2. Selection sort.
+
+3. Quick sort.
